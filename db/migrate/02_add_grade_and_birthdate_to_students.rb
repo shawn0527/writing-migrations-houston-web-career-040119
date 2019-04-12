b5155d1531b68bac2f4afe63e6346fe964261112
Shawn Ren <shawnren527@gmail.com>
2019-04-12 19:40:28 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,1 +1,4 @@
-class AddAgeandBirthdate
+class AddColumnToStudents < ActiveRecord::Migration[5.2]
+  def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
